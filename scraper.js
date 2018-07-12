3faeb0c47fbff6c4240e946c8dd6ebd6e9973351
Sharina V. Jones <sharinavjones@gmail.com>
2018-07-12 01:46:36 -0400

    finished error handling

--- scraper.js
+++ scraper.js
@@ -6,39 +6,46 @@ Cheerio: https://www.npmjs.com/package/cheerio
 JSON2csv: https://www.npmjs.com/package/json2csv
 */
 
-
-
 //import required files
 const fs = require('fs');
+//scraper
 const request = require('request');
+//allows for jquery syntax
 const cheerio = require('cheerio');
+//parses data to csv
 const Json2csvParser = require('json2csv').Parser;
 //Require http module for status codes
 const http = require('http');
 
 //check for the existance of a file
+  //creates file if it doesn't exist
 if (!fs.existsSync('./data')) {
     fs.mkdirSync('./data');
 }
 
-let body;
-//stores shirt links
-const links = [];
+//declarations
+const entrance = "http://shirts4mike.com/shirts.php";
+const links = []; //stores individual shirt links
 
-request('http://shirts4mike.com/shirts.php', function(error, response, body) {
+//initial scrape
+  //retrieves all shirts from page
+request(entrance, function(error, response, body) {
+    //checks for valid response
     if (!error && response.statusCode === 200) {
         let $ = cheerio.load(body);
-        const products = $('.products li');      
+        //gets individual page links
+        const products = $('.products li');
+        $(products).each(function() {
+            //stores links in array
+            let url = $(this).children().first().attr('href');
+            links.push(url);
+        });      
 
-      $(products).each(function() {
-          let url = $(this).children().first().attr('href');
-          links.push(url);
-      });      
-
-      getShirtData(links);
+        getShirtData(links);
     } else {
+        //generates errors
         if (response) {
-            const message = "The following error occurred " + http.STATUS_CODES[response.statusCode];
+            const message = `Unable to connect to ${entrance}: ${response.statusCode} (${http.STATUS_CODES[response.statusCode]})`;
             const statusCodeError = new Error(message);
             printError(statusCodeError);
         } else {
@@ -49,10 +56,13 @@ request('http://shirts4mike.com/shirts.php', function(error, response, body) {
     }
 });
 
+//second scrape
+  //gets info from individual shirts
 function getShirtData(links) {
     let i = 0;  
     let shirtData = [];
 
+    //recursively calls itself until each shirt's info is obtained
     function next() {
         if (i < links.length) {
             request(`http://shirts4mike.com/${links[i]}`, function(error, response, body) {
@@ -72,9 +82,7 @@ function getShirtData(links) {
                     return next();
                 } else {
                     if (response) {
-                        const message = "The following error occurred " + http.STATUS_CODES[response.statusCode];
-                        const statusCodeError = new Error(message);
-                        printError(statusCodeError);
+                        const message = `Unable to connect to http://shirts4mike.com/${links[i]}: ${response.statusCode} (${http.STATUS_CODES[response.statusCode]})`;
                     } else {
                         const message = `The following error occurred: ${error.code} (${error.message})`;
                         const statusCodeError = new Error(message);
@@ -84,15 +92,18 @@ function getShirtData(links) {
             });
 
         } else {
+            //parses shirt info into csv
             const fields = ['title', 'price', 'imageURL', 'url', 'time'];
             const json2csvParser = new Json2csvParser({ fields });
             const csv = json2csvParser.parse(shirtData);
             
-            console.log(csv);
+            //creates a filename using the current day
             const currentDate = new Date();
             const fileName = `${currentDate.getFullYear()}-${currentDate.getMonth()}-${currentDate.getDay()}`;
             const filePath = `./data/${fileName}`;
 
+            //writes to a file
+              //creates file if it doesn't exist
             fs.writeFileSync(filePath, csv); 
         }
     }
